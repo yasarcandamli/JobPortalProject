d8e92fc5a1fb4ea10ff6bc8a6631e88641a158f4
yasarcandamli <yasarcandamli@gmail.com>
2024-10-18 21:47:39 +0300

    Create Recruiter Profile - Controller and Service

---
+++ src/main/java/com/yasarcan/jobportal/services/RecruiterProfileService.java
@@ -0,0 +1,23 @@
+package com.yasarcan.jobportal.services;
+
+import com.yasarcan.jobportal.entity.RecruiterProfile;
+import com.yasarcan.jobportal.repository.RecruiterProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+
+    private final RecruiterProfileRepository recruiterProfileRepository;
+
+    @Autowired
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id) {
+        return recruiterProfileRepository.findById(id);
+    }
+}

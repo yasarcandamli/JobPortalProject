129534eb021d7b8f29c0e0ec8e9a36f6cfb248a0
yasarcandamli <yasarcandamli@gmail.com>
2024-10-22 13:10:38 +0300

    Display and Retrieve Jobs

--- src/main/java/com/yasarcan/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/yasarcan/jobportal/controller/JobPostActivityController.java
@@ -1,12 +1,15 @@
 package com.yasarcan.jobportal.controller;
 
 import com.yasarcan.jobportal.entity.JobPostActivity;
+import com.yasarcan.jobportal.entity.RecruiterJobsDto;
+import com.yasarcan.jobportal.entity.RecruiterProfile;
 import com.yasarcan.jobportal.entity.Users;
 import com.yasarcan.jobportal.services.JobPostActivityService;
 import com.yasarcan.jobportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobPostActivityController {
@@ -35,6 +39,11 @@ public class JobPostActivityController {
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
             String currentUsername = authentication.getName();
             model.addAttribute("username", currentUsername);
+            if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))) {
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService
+                        .getRecruiterJobs(((RecruiterProfile) currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost", recruiterJobs);
+            }
         }
 
         model.addAttribute("user", currentUserProfile);
---
+++ src/main/java/com/yasarcan/jobportal/entity/IRecruiterJobs.java
@@ -0,0 +1,14 @@
+package com.yasarcan.jobportal.entity;
+
+public interface IRecruiterJobs {
+
+    Long getTotalCandidates();
+    int getJob_post_id();
+    String getJob_title();
+    int getLocationId();
+    String getCity();
+    String getState();
+    String getCountry();
+    int getCompanyId();
+    String getName();
+}
